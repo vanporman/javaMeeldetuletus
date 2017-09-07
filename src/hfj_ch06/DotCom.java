5a40a29535a44d3c9702d180b52a762e486bf09d
Andreas Porman <andreas.porman@datel.ee>
2017-09-07 12:36:39 +0300

    kolmas

---
+++ src/hfj_ch06/DotCom.java
@@ -0,0 +1,35 @@
+package hfj_ch06;
+
+import java.util.ArrayList;
+
+public class DotCom {
+	private ArrayList<String> locationCells;
+	private String name;
+    
+    public void setLocationCells(ArrayList<String> loc)
+    {
+        locationCells = loc;
+    }
+    
+    public void setName(String n){
+    	name = n;
+    }
+    
+    public String checkYourself(String userInput){
+        String result = "miss";
+        int index = locationCells.indexOf(userInput);
+        if (index >= 0) {
+            locationCells.remove(index);
+            if (locationCells.isEmpty()) {
+                result = "kill";
+                System.out.println("You sunk " + name);
+            }
+            else
+            {
+                result = "hit";
+            }
+        }
+        return result;
+    }
+
+}
